c435d53722e548be287b35fb219fc0bbc8fe97ef
lestrrat <lestrrat+github@gmail.com>
2012-03-26 17:08:03 +0900

    add test tools for build.zero.mq

---
+++ test/autobuild.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ABSPATH=$(cd ${0%/*} && echo $PWD/${0##*/})
+THISDIR=$(dirname $ABSPATH)
+export CPANM=$THISDIR/cpanm
+export PERL=(which perl)
+
+cd $1
+
+$PERL $CPANM -lextlib \
+    Module::Install \
+    Module::Install::AuthorTests \
+    Module::Install::CheckLib \
+    Module::Install::XSUtil \
+    AnyEvent
+
+$PERL Makefile.PL -g
+$PERL $CPANM -lextlib --installdeps -lextlib .
+
+make test
+
